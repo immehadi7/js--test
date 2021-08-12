ec83c8ff347ff96de657b18572bc3a0500094fb6
immehadi7 <mehedy.adi7@gmail.com>
2021-08-12 23:02:39 +0600

    last commit

---
+++ js/scrpt.js
@@ -0,0 +1,28 @@
+var headingg2 = document.getElementById("heading2");headingg2.innerHTML = "Game on , Lets go -- Good Bye good bye ";
+
+console.log("hello world");
+
+   /* var bikebrand = prompt("Which brand bike do you like most?")
+document.getElementById('someText').innerText= bikebrand ;  */
+
+    /* get element by id */
+    document.getElementById('blogss').innerText = "Our blog";  
+
+    /* get elements by tag name */ 
+    document.getElementsByTagName('h2')[1].innerHTML= 'No! Tomorrow is Friday.'
+
+    /* get elemnts by class */
+    document.getElementsByClassName("sir")[0].innerHTML= "we have best programming teacher like  : anisul Sir best";
+
+    document.getElementsByClassName('sir')[1].innerHTML = " hmmm , mahbub Sir o  best" ;
+    /* query selector */document.querySelector('#para-id').innerHTML="In sha Allah ,Amra programmer ra akdin bissho kapabo. ";
+
+
+    document.querySelector('.para').innerHTML= 'In sha Allah. We are the students of programming hero batch 04' ;
+
+    document.querySelectorAll("p")[2].innerHTML = 'para nai sob easy , when Anis sir teaches anything' ;
+
+
+    document.querySelector("nav a").innerHTML= "Homes"; 
+    document.querySelector("ul li a").innerHTML="Buy right now";
+    document.querySelectorAll('ul li a')[1].innerHTML='Services';
